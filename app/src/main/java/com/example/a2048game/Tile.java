31b8236a38fac614b01453baa0f4b99fb078827b
Kristel <kguzmanv@ucenfotec.ac.cr>
2020-02-15 20:16:19 -0600

    agrega firebase

---
+++ app/src/main/java/com/example/a2048game/Tile.java
@@ -0,0 +1,13 @@
+package com.example.a2048game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
