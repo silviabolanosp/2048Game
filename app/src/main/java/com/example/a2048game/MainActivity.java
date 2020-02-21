b79e87302481e48a77ad9248b34f0420c57c8c8e
silviabolanosp <silviabolanosp@gmail.com>
2020-02-20 20:18:09 -0600

    main activity java

--- app/src/main/java/com/example/a2048game/MainActivity.java
+++ app/src/main/java/com/example/a2048game/MainActivity.java
@@ -9,11 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.widget.NumberPicker;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView username;
     Button btnStart;
+    MediaPlayer mediaPlayer;
+    Switch timerSwitch;
+    NumberPicker np;
+    int minutes = 0;
+
+    public static final String EXTRA_MINUTES = "com.example.a2048game.MINUTES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +32,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         username= (TextView) findViewById(R.id.user);
+        timerSwitch = (Switch) findViewById(R.id.timer);
 
         btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = username.getText().toString();
+
+                if (timerSwitch.isChecked())
+                    minutes = np.getValue();
+                else
+                    minutes = 0;
+
                 Intent nextView = new Intent(MainActivity.this,GameActivity.class);
                 nextView.putExtra("nameGame",name);
+                nextView.putExtra(EXTRA_MINUTES, minutes);
                 startActivity(nextView);
             }
         });
+
+
+
+        //Get the widgets reference from XML layout
+        final TextView tv = (TextView) findViewById(R.id.tv);
+        np = (NumberPicker) findViewById(R.id.np);
+
+        //Set TextView text color
+        tv.setTextColor(Color.parseColor("#ffd32b3b"));
+
+        //Populate NumberPicker values from minimum and maximum value range
+        //Set the minimum value of NumberPicker
+        np.setMinValue(1);
+        //Specify the maximum value/number of NumberPicker
+        np.setMaxValue(30);
+
+        //Gets whether the selector wheel wraps when reaching the min/max value.
+        np.setWrapSelectorWheel(true);
+
+        //Set a value change listener for NumberPicker
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+                //Display the newly selected number from picker
+                tv.setText("Minutos : " + newVal);
+            }
+        });
+
+        // MUSIC
+        //mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+        //mediaPlayer.start();
     }
 
 
@@ -53,4 +102,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }*/
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+
+    }
 }
